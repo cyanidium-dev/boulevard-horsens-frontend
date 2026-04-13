94b2546fe049c1677f12b5d2f419a807d5b1144e
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-04-13 18:16:36 +0300

    added breadcrumbs

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,9 +1,13 @@
 import Hero from "@/components/servicesPage/hero/Hero";
+import { Breadcrumbs } from "@/components/shared/breadcrumbs/Breadcrumbs";
 
 export default function ServicesPage() {
+  const breadcrumbSteps = [{ label: `Hjem`, href: `/` }, { label: "Services" }];
+
   return (
     <>
       <Hero />
+      <Breadcrumbs steps={breadcrumbSteps} />
     </>
   );
 }
