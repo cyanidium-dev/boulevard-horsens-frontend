4642cc75e6f9f80c4fd883fff77d0a11b1082d4d
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-04-14 14:53:59 +0300

    added hero section

--- src/components/homePage/hero/Hero.tsx
+++ src/components/homePage/hero/Hero.tsx
@@ -1,5 +1,7 @@
+import Button from "@/components/shared/buttons/Button";
 import HeroGallerySlider from "./HeroGallerySlider";
 import { HERO_GALLERY_IMAGES } from "./heroImages";
+import Container from "@/components/shared/container/Container";
 
 export default function Hero() {
   return (
@@ -17,6 +19,27 @@ export default function Hero() {
       <div className="absolute -z-20 inset-0 w-full h-full rounded-b-[36px] overflow-hidden">
         <HeroGallerySlider images={HERO_GALLERY_IMAGES} />
       </div>
+
+      <Container className="pt-[134px] lg:pt-[151px]">
+        <h1 className="max-w-[517px] mb-9 font-evolenta text-[40px] lg:text-[72px] leading-[120%] font-normal uppercase text-beige">
+          Beauty Salon Boulevard i Horsens
+        </h1>
+        <p className="mb-20 lg:mb-[54px] max-w-[270px] text-beige">
+          Ansigtsbehandlinger, lash lift, brow lamination, manicure, pedicure og
+          voks
+        </p>
+        {/* <p>
+          Professionelle behandlinger med fokus på kvalitet, naturlige
+          resultater og individuel tilgang
+        </p> */}
+        <Button
+          href="/#services"
+          variant="beige"
+          className="w-full sm:w-[270px] h-15 lg:h-[68px]"
+        >
+          Vælg behandling
+        </Button>
+      </Container>
     </section>
   );
 }
