ef136888977f6ccf3f5c0f1f90a6eaa7a57e4cdd
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-04-16 10:44:19 +0300

    added prices section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,20 @@ import Prices from "@/components/homePage/prices/Prices";
 import About from "@/components/homePage/about/About";
 import MarqueeLine from "@/components/shared/marquee/MarqueeLine";
 import Services from "@/components/homePage/services/Services";
-import { SERVICES_QUERY } from "@/lib/queries";
+import { SERVICES_QUERY, WORKING_HOURS_QUERY } from "@/lib/queries";
 import { Service } from "@/types/service";
 import { fetchSanityData } from "@/utils/fetchSanityData";
 import Course from "@/components/homePage/course/Course";
 import Results from "@/components/homePage/results/Results";
 
+interface WorkingHours {
+  from?: string;
+  to?: string;
+}
+
 export default async function HomePage() {
   const services = await fetchSanityData<Service[]>(SERVICES_QUERY);
+  const workingHours = await fetchSanityData<WorkingHours>(WORKING_HOURS_QUERY);
 
   return (
     <>
@@ -19,7 +25,7 @@ export default async function HomePage() {
       <Results />
       <MarqueeLine variant="black" className="mb-14" />
       <Course />
-      <Prices />
+      <Prices from={workingHours?.from} to={workingHours?.to} />
       <About />
       <MarqueeLine variant="black" className="mb-9 lg:mb-[116px]" />
     </>
--- src/components/homePage/prices/Prices.tsx
+++ src/components/homePage/prices/Prices.tsx
@@ -1,48 +1,75 @@
 import Container from "@/components/shared/container/Container";
 import Image from "next/image";
 import Button from "@/components/shared/buttons/Button";
+import StarIcon from "@/components/shared/icons/StarIcon";
 
-export default function Prices() {
+interface PricesProps {
+  from?: string;
+  to?: string;
+}
+
+export default function Prices({ from, to }: PricesProps) {
+  const workingHours = from && to ? `${from} - ${to}` : "09:00 - 18:00";
+
   return (
     <section id="prices" className="pb-20 lg:pb-[116px]">
-      <Container className="relative pt-15 pb-5 lg:pb-[65px] rounded-[36px] overflow-hidden">
+      <Container className="relative rounded-[36px] overflow-hidden">
         <Image
           src="/images/homePage/price/image.webp"
           alt="Price"
           fill
-          className="scale-x-[-1] -z-10 object-cover object-[center_60%]"
+          className="-z-10 object-cover object-left rounded-[36px]"
         />
-        <div className="flex flex-col gap-20">
-          <div>
-            <h2 className="mb-7 lg:mb-[54px] font-evolenta text-[36px] lg:text-[64px] leading-[120%] font-normal uppercase text-beige">
-              Priser på behandlinger
-            </h2>
-            <Button
-              href="/services"
-              variant="black"
-              className="w-full sm:h-[238px] h-[68px]"
-            >
-              Se priser
-            </Button>
-          </div>
+        <div className="pt-15 pb-5 lg:pb-[65px]">
+          {" "}
+          <div className="flex flex-col lg:flex-row gap-20">
+            <div>
+              <h2 className="mb-7 lg:mb-[54px] font-evolenta text-[36px] lg:text-[64px] leading-[120%] font-normal uppercase text-beige">
+                Priser på behandlinger
+              </h2>
+              <Button
+                href="/services"
+                variant="black"
+                className="w-full sm:w-[238px] h-[68px]"
+              >
+                Se priser
+              </Button>
+            </div>
 
-          <div className="relative pt-9 pb-6 px-6 rounded-[28px] bg-white/10 backdrop-blur-[10px]">
-            <div
-              className="absolute inset-0 rounded-[28px] pointer-events-none"
-              style={{
-                background:
-                  "linear-gradient(270.67deg, #F2F2F2 -9.58%, #C7C7C7 103.45%)",
-                padding: "1px",
-                WebkitMask:
-                  "linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)",
-                WebkitMaskComposite: "xor",
-                maskComposite: "exclude",
-              }}
-            />
-            <h3 className="mb-9 lg:mb-12 font-evolenta text-[24px] leading-[120%] font-normal uppercase text-beige">
-              Priser på behandlinger
-            </h3>
-            <div className="py-9 lg:py-3.5 px-7 lg:px-[45px] rounded-[16px] bg-beige"></div>
+            <div className="relative">
+              <StarIcon className="hidden lg:block absolute -z-10 top-[-25px] right-[-22px] lg:size-[67px] text-beige" />
+              <div className="relative pt-9 pb-6 px-6 rounded-[28px] bg-white/10 shadow-[inset_0px_4px_12.6px_0px_rgba(255,255,255,0.25)] backdrop-blur-[10px]">
+                <StarIcon className="hidden lg:block absolute z-10 bottom-[-30px] left-[-30px] lg:size-[67px] text-beige" />
+                <div
+                  className="absolute inset-0 rounded-[28px] pointer-events-none"
+                  style={{
+                    background:
+                      "linear-gradient(270.67deg, #F2F2F2 -9.58%, #C7C7C7 103.45%)",
+                    padding: "1px",
+                    WebkitMask:
+                      "linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)",
+                    WebkitMaskComposite: "xor",
+                    maskComposite: "exclude",
+                  }}
+                />
+                <h3 className="mb-9 lg:mb-12 font-evolenta text-[32px] leading-[120%] font-normal uppercase text-beige">
+                  Du kan komme både med og uden tidsbestilling.
+                </h3>
+                <div className="lg:flex py-9 lg:py-3.5 px-7 lg:px-0 rounded-[16px] bg-beige font-evolenta text-[20px] lg:text-[32px] leading-[120%] uppercase text-black text-center">
+                  <h4 className="lg:w-1/2 mb-9  h-fit lg:my-auto font-evolenta text-[20px] leading-[120%] font-normal uppercase text-black">
+                    Åbningstider
+                  </h4>
+                  <div className="relative mb-9 lg:mb-0 lg:w-[1px] shrink-0">
+                    <div className="absolute -top-[2px] -left-0.5 size-[5px] bg-black rounded-full" />
+                    <div className="h-[1px] lg:w-[1px] lg:h-[69px] bg-black" />
+                    <div className="absolute -bottom-[2px] -right-0.5 size-[5px] bg-black rounded-full" />
+                  </div>
+                  <p className="lg:w-1/2 h-fit lg:my-auto font-evolenta text-[20px] leading-[120%] font-normal uppercase text-black">
+                    {workingHours}
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </Container>
