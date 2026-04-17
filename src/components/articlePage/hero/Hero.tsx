3ce348e78900d342e28cd9ed21ca86438978cfa7
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-04-17 10:57:21 +0300

    updated hero styles

--- src/components/articlePage/hero/Hero.tsx
+++ src/components/articlePage/hero/Hero.tsx
@@ -44,30 +44,21 @@ export default function Hero({ article }: HeroProps) {
       <div
         className="absolute inset-0 -z-10"
         style={{
-          background: `
-      linear-gradient(
-        0deg,
-        rgba(0, 0, 0, 0) -36.89%,
-        rgba(0, 0, 0, 0.464) 91.14%,
-        rgba(0, 0, 0, 0) 144.39%
-      ),
-      linear-gradient(
-        240.18deg,
-        rgba(0, 0, 0, 0) 19.24%,
-        rgba(0, 0, 0, 0.8) 82.96%
-      )
-    `,
+          background: `linear-gradient(0deg, rgba(0, 0, 0, 0) -36.89%, rgba(0, 0, 0, 0.464) 91.14%, rgba(0, 0, 0, 0) 144.39%),
+                       linear-gradient(240.18deg, rgba(0, 0, 0, 0) 19.24%, rgba(0, 0, 0, 0.8) 82.96%)`,
         }}
       ></div>
-      <Container className="pt-[147px] lg:pt-[179px] pb-7 lg:pb-[108px]">
-        <PageTitle className="max-w-[978px] mb-6 lg:mb-9">{heroTitle}</PageTitle>
+      <Container className="pt-[147px] lg:pt-[179px] pb-10 lg:pb-[108px]">
+        <PageTitle className="max-w-[978px] mb-6 lg:mb-9 text-beige">
+          {heroTitle}
+        </PageTitle>
         <motion.p
           initial="hidden"
           whileInView="visible"
           exit="exit"
           viewport={{ once: true, amount: 0.1 }}
           variants={fadeInAnimation({ scale: 0.85, x: 70, y: 30, delay: 0.4 })}
-          className="max-w-[978px] whitespace-pre-line"
+          className="max-w-[978px] whitespace-pre-line text-beige"
         >
           {heroDescription}
         </motion.p>
