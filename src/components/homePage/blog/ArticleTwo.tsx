e16702f66b1420911762c7d6f40c0c9bd997aa9d
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-04-17 14:53:47 +0300

    added blog section

---
+++ src/components/homePage/blog/ArticleTwo.tsx
@@ -0,0 +1,63 @@
+import { BlogPost } from "@/types/blogPost";
+import Image from "next/image";
+import Link from "next/link";
+import * as motion from "motion/react-client";
+import { fadeInAnimation } from "@/utils/animationVariants";
+import { urlForSanityImage } from "@/utils/getUrlForSanityImage";
+import EstimatedReadingTime from "@/components/shared/estReadingTime/EstimatedReadingTime";
+
+interface ArticleTwoProps {
+  post: BlogPost;
+}
+
+export default function ArticleTwo({ post }: ArticleTwoProps) {
+  const { heroTitle, heroDescription, heroMobileImage, slug } = post;
+
+  return (
+    <Link
+      href={`/blog/${slug}`}
+      className="block xl:w-[calc(50%-7px)] h-full rounded-[20px] overflow-hidden"
+    >
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        exit="exit"
+        viewport={{ once: true, amount: 0.3 }}
+        variants={fadeInAnimation({ y: 20 })}
+        className="relative flex flex-col h-full px-4 py-5 bg-white/10 shadow-[inset_0px_4px_12.6px_0px_rgba(255,255,255,0.25)] backdrop-blur-[10px]"
+      >
+        <div
+          className="absolute inset-0 rounded-[20px] pointer-events-none"
+          style={{
+            background:
+              "linear-gradient(270.67deg, #F2F2F2 -9.58%, #C7C7C7 103.45%)",
+            padding: "1px",
+            WebkitMask:
+              "linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)",
+            WebkitMaskComposite: "xor",
+            maskComposite: "exclude",
+          }}
+        />
+        <div className="flex-grow relative w-full mb-4 rounded-[12px] overflow-hidden">
+          <Image
+            src={urlForSanityImage(heroMobileImage).fit("crop").url()}
+            alt={heroTitle}
+            fill
+            className="object-cover"
+          />
+        </div>
+
+        <h3 className="mb-5 font-evolenta text-[15px] font-normal leading-[133%] uppercase">
+          {heroTitle}
+        </h3>
+
+        <p className="mb-6 md:mb-5 text-[12px] font-light leading-[120%] line-clamp-5 md:line-clamp-5">
+          {heroDescription}
+        </p>
+        <div className="flex gap-3 items-center flex-wrap">
+          <EstimatedReadingTime post={post} />
+        </div>
+      </motion.div>
+    </Link>
+  );
+}
--- src/components/homePage/blog/Blog.tsx
+++ src/components/homePage/blog/Blog.tsx
@@ -1,11 +1,34 @@
 import Container from "@/components/shared/container/Container";
 import Image from "next/image";
+import { Suspense } from "react";
+import Loader from "@/components/shared/loader/Loader";
+import { fetchSanityData } from "@/utils/fetchSanityData";
+import { ALL_BLOG_POSTS_QUERY } from "@/lib/queries";
+import { BlogPost } from "@/types/blogPost";
+import BlogList from "@/components/homePage/blog/BlogList";
+import * as motion from "motion/react-client";
+import { fadeInAnimation } from "@/utils/animationVariants";
 
-export default function Blog() {
+export default async function Blog() {
+  const posts = await fetchSanityData<BlogPost[]>(ALL_BLOG_POSTS_QUERY);
+
+  const postsList = posts.slice(0, 3);
+
   return (
     <section className="pb-[55px] lg:pb-15">
       <Container>
-        <div className="lg:flex justify-between items-center mb-10 lg:mb-8">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          exit="exit"
+          viewport={{ once: true, amount: 0.1 }}
+          variants={fadeInAnimation({
+            delay: 0.2,
+            x: -40,
+            duration: 1,
+          })}
+          className="lg:flex justify-between items-center mb-10 lg:mb-8"
+        >
           <h2 className="max-w-[472px] lg:max-w-[562px] xl:max-w-[762px] font-evolenta text-[24px] lg:text-[48px] leading-[120%] font-normal uppercase">
             Læs vores nyeste artikler om hudpleje og behandlinger
           </h2>
@@ -16,7 +39,10 @@ export default function Blog() {
             height={68}
             className="hidden lg:block w-[288px] h-[68px]"
           />
-        </div>
+        </motion.div>
+        <Suspense fallback={<Loader />}>
+          <BlogList postsList={postsList} />
+        </Suspense>
       </Container>
     </section>
   );
