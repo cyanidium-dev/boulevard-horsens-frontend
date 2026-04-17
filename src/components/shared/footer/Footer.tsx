6cfa2be11b2440cb1ee8c6355e6842b39c3d521a
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-04-17 17:10:35 +0300

    added styles for desktop

--- src/components/shared/footer/Footer.tsx
+++ src/components/shared/footer/Footer.tsx
@@ -19,7 +19,7 @@ export default function Footer() {
     <footer className="relative overflow-hidden">
       <MarqueeLine className="" />
       <Container className="relative">
-        <div className="absolute top-[21px] right-0 w-[73px] h-[559px]">
+        <div className="lg:hidden absolute top-[21px] right-0 w-[73px] h-[559px]">
           <Image
             src="/images/footer/logo-mob.webp"
             alt="footer background"
@@ -28,7 +28,16 @@ export default function Footer() {
           />
         </div>
 
-        <div className="absolute top-[-71px] right-[68px] w-[186px] h-[259px]">
+        <div className="hidden lg:block absolute bottom-[0px] left-[-11px] w-[1291px] h-[173px]">
+          <Image
+            src="/images/footer/logo-desk.webp"
+            alt="footer background"
+            fill
+            className="-z-10 object-cover"
+          />
+        </div>
+
+        <div className="lg:hidden absolute top-[-71px] right-[68px] w-[186px] h-[259px]">
           <Image
             src="/images/footer/spatulas.webp"
             alt="footer background"
@@ -37,7 +46,16 @@ export default function Footer() {
           />
         </div>
 
-        <div className="absolute bottom-0 right-0 w-[341px] h-[418px]">
+        <div className="hidden lg:block absolute top-[-71px] right-[-188px] w-[354px] h-[313px]">
+          <Image
+            src="/images/footer/spatulas-desk.webp"
+            alt="footer background"
+            fill
+            className="-z-10 object-cover"
+          />
+        </div>
+
+        <div className="lg:hidden absolute bottom-0 right-0 w-[341px] h-[418px]">
           <Image
             src="/images/footer/hand-mob.webp"
             alt="footer background"
@@ -46,77 +64,82 @@ export default function Footer() {
           />
         </div>
 
-        <div className="hidden absolute bottom-0 right-[-143px] w-[519px] h-[519px]">
+        <div className="hidden lg:block absolute bottom-0 left-[0px] w-[666px] h-[522px]">
           <Image
             src="/images/footer/hand.webp"
             alt="footer background"
             fill
-            className="-z-10 object-cover"
+            className="-z-20 object-cover"
           />
         </div>
 
-        <div className="flex flex-col gap-[54px] pt-25 lg:pt-[116px] pb-[127px] lg:pb-[227px]">
-          <div>
-            <h3 className="text-[14px] leading-[120%] font-light font-evolenta mb-3.5">
-              Arbejdsplan
-            </h3>
-            <p className="text-[12px] font-semibold leading-[120%] uppercase">
-              man-fre{" "}
-            </p>
-            <p className="text-[12px] font-semibold leading-[120%] uppercase">
-              10.00-19.00
-            </p>
-            <p className="text-[12px] font-semibold leading-[120%] uppercase">
-              lør-søn
-            </p>
-            <p className="text-[12px] font-semibold leading-[120%] uppercase">
-              12.00-18.00
-            </p>
+        <div className="flex flex-col lg:flex-row-reverse lg:justify-end gap-[54px] pt-25 lg:pt-[116px] pb-[127px] lg:pb-[227px]">
+          <div className="flex flex-col gap-[54px] lg:flex-row ">
+            <div className="lg:w-[118px]">
+              <h3 className="text-[14px] leading-[120%] font-light font-evolenta mb-3.5">
+                Arbejdsplan
+              </h3>
+              <p className="text-[12px] font-semibold leading-[120%] uppercase">
+                man-fre{" "}
+              </p>
+              <p className="mb-3 text-[12px] font-semibold leading-[120%] uppercase">
+                10.00-19.00
+              </p>
+              <p className="text-[12px] font-semibold leading-[120%] uppercase">
+                lør-søn
+              </p>
+              <p className="text-[12px] font-semibold leading-[120%] uppercase">
+                12.00-18.00
+              </p>
+            </div>
+            <div>
+              <h3 className="text-[14px] leading-[120%] font-light font-evolenta mb-3.5">
+                Vores adresse
+              </h3>
+              <a
+                rel="noopener noreferrer nofollow"
+                href={ADDRESS_URL}
+                target="_blank"
+                className="block max-w-[148px] text-[12px] font-semibold leading-[120%] uppercase"
+              >
+                {ADDRESS}
+              </a>
+            </div>
+
+            <div className="flex flex-col gap-[54px] lg:gap-[27px]">
+              <div>
+                <h3 className="text-[14px] leading-[120%] font-light font-evolenta mb-3.5">
+                  Cvr-nr.:
+                </h3>
+                <p className="text-[12px] font-semibold leading-[120%] uppercase">
+                  {CVR_NUMBER}
+                </p>
+              </div>
+              <div>
+                <h3 className="text-[14px] leading-[120%] font-light font-evolenta mb-3.5">
+                  Eller skriv til os:
+                </h3>
+                <a
+                  rel="noopener noreferrer nofollow"
+                  href={`mailto:${EMAIL}`}
+                  target="_blank"
+                  className="block text-[12px] font-semibold leading-[120%] uppercase"
+                >
+                  {EMAIL}
+                </a>
+              </div>
+            </div>
           </div>
 
-          <div>
-            <h3 className="text-[14px] leading-[120%] font-light font-evolenta mb-3.5">
-              Vores adresse
-            </h3>
-            <a
-              rel="noopener noreferrer nofollow"
-              href={ADDRESS_URL}
-              target="_blank"
-              className="block max-w-[148px] text-[12px] font-semibold leading-[120%] uppercase"
-            >
-              {ADDRESS}
-            </a>
+          <div className="flex flex-col lg:flex-row lg:items-center gap-[54px] xl:gap-[271px]">
+            <SocialsGroup />
+            <Button href="/blog" className="sm:w-[182px] h-[52px]">
+              Vores blog
+            </Button>
           </div>
-
-          <div>
-            <h3 className="text-[14px] leading-[120%] font-light font-evolenta mb-3.5">
-              Cvr-nr.:
-            </h3>
-            <p className="text-[12px] font-semibold leading-[120%] uppercase">
-              {CVR_NUMBER}
-            </p>
-          </div>
-
-          <div>
-            <h3 className="text-[14px] leading-[120%] font-light font-evolenta mb-3.5">
-              Eller skriv til os:
-            </h3>
-            <a
-              rel="noopener noreferrer nofollow"
-              href={`mailto:${EMAIL}`}
-              target="_blank"
-              className="block text-[12px] font-semibold leading-[120%] uppercase"
-            >
-              {EMAIL}
-            </a>
-          </div>
-
-          <SocialsGroup />
-
-          <Button href="/blog">Vores blog</Button>
         </div>
 
-        <div className="absolute bottom-8 left-5">
+        <div className="absolute bottom-8 lg:bottom-18 left-5 lg:left-auto lg:right-7">
           <p className="mb-3 text-[8px] leading-[120%] font-medium uppercase">
             Created by:
           </p>
