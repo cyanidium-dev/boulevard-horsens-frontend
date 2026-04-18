4a914dd0a478fe3b94122034a1c8e83d7b8c11e1
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-04-18 10:37:49 +0300

    added heading

--- src/components/homePage/results/Results.tsx
+++ src/components/homePage/results/Results.tsx
@@ -1,10 +1,21 @@
 import Container from "@/components/shared/container/Container";
+import Image from "next/image";
 
 export default function Results() {
   return (
     <section className="mb-[54px]">
       <Container>
-        <h2>Se resultater fra vores kunder før og efter behandlinger</h2>
+        <div className="flex flex-col gap-8 md:flex-row md:items-center md:justify-between">
+          <Image
+            src="/images/homePage/results/decor.webp"
+            alt="Results"
+            width={270}
+            height={116}
+          />
+          <h2 className="md:max-w-[380px] lg:max-w-[600px] xl:max-w-[825px] font-evolenta text-[26px] lg:text-[48px] leading-[120%] font-light text-right uppercase text-black">
+            Se resultater fra vores kunder før og efter behandlinger
+          </h2>
+        </div>
       </Container>
     </section>
   );
