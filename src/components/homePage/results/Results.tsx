8070b716478ccb4805a6792db64e7ccf8e4eb788
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-04-18 10:26:16 +0300

    added hero animation

--- src/components/homePage/results/Results.tsx
+++ src/components/homePage/results/Results.tsx
@@ -2,8 +2,10 @@ import Container from "@/components/shared/container/Container";
 
 export default function Results() {
   return (
-    <section>
-      <Container></Container>
+    <section className="mb-[54px]">
+      <Container>
+        <h2>Se resultater fra vores kunder før og efter behandlinger</h2>
+      </Container>
     </section>
   );
 }
